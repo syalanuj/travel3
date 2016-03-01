00ea34448730053c92b715795e7290b01afb470f
Anuj Syal <syal.anuj@gmail>
2016-03-01 23:05:51 +0530

    Fix

--- app/components/account/profile/profileCtrl.js
+++ app/components/account/profile/profileCtrl.js
@@ -1,9 +1,8 @@
 (function () {
     'use strict';
-
     var app = angular.module('campture');
-    app.controller('ProfileCtrl', ['$scope', '$cookies', '$rootScope', 'AccountService', 'uiGmapIsReady', '$routeParams', '$timeout','$location', controller]);
-    function controller($scope, $cookies, $rootScope, accountService, uiGmapIsReady, $routeParams, $timeout,$location) {
+    app.controller('ProfileCtrl', ['$scope', '$cookies', '$rootScope', 'AccountService', 'uiGmapIsReady', '$routeParams', '$timeout', '$location', controller]);
+    function controller($scope, $cookies, $rootScope, accountService, uiGmapIsReady, $routeParams, $timeout, $location) {
         //====== Scope Variables==========
         //================================  
         $scope.isPageLoading = false;
@@ -36,27 +35,37 @@
             }
         });
         accountService.getMyTrips($routeParams.userId, function (data) {
-            $scope.$apply(function () {
-                if (data.length > 0) {
-                    var markerId = 0;
-                    $scope.myTrips = data;
-                    $scope.isPageLoading = true;
-                    angular.forEach($scope.myTrips, function (trip, key) {
-                        var initUrl = trip.main_image ? trip.main_image.image_url : trip.visited_places[0].images[0].image_url;
-                        trip.cropped_image_url = $rootScope.getCroppedTripImageUrl(initUrl);
-                        angular.forEach(trip.visited_places, function (place, key) {
-                            $scope.allMarkers.push({ latitude: place.coordinates.latitude, longitude: place.coordinates.longitude, title: place.location, id: markerId })
-                            var latlng = new google.maps.LatLng(place.coordinates.latitude, place.coordinates.longitude);
-                            bounds.extend(latlng);
-                            angular.forEach(place.images, function (image, key) {
-                                $scope.allTripImages.push(image);
+            try {
+                $scope.$apply(function () {
+                    if (data.length > 0) {
+                        var markerId = 0;
+                        $scope.myTrips = data;
+                        $scope.isPageLoading = true;
+                        angular.forEach($scope.myTrips, function (trip, key) {
+                            var initUrl = trip.main_image ? trip.main_image.image_url : trip.visited_places[0].images[0].image_url;
+                            trip.cropped_image_url = $rootScope.getCroppedTripImageUrl(initUrl);
+                            angular.forEach(trip.visited_places, function (place, key) {
+                                try {
+                                    $scope.allMarkers.push({ latitude: place.coordinates.latitude, longitude: place.coordinates.longitude, title: place.location, id: markerId })
+                                    var latlng = new google.maps.LatLng(place.coordinates.latitude, place.coordinates.longitude);
+                                    bounds.extend(latlng);
+                                }
+                                catch (e) {
+                                    console.log(e);
+                                }
+                                angular.forEach(place.images, function (image, key) {
+                                    $scope.allTripImages.push(image);
+                                });
+                                markerId++;
                             });
-                            markerId++;
                         });
-                    });
-                }
-                $scope.isPageLoading = true;
-            });
+                    }
+                    $scope.isPageLoading = true;
+                });
+            }
+            catch (e) {
+                console.log(e);
+            }
         });
         $scope.$on('mapInitialized', function (event, map) {
             map.setCenter(bounds.getCenter());
