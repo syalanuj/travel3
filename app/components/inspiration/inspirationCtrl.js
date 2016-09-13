26f80fda6664913175c97b98bbffb3624f564182
Anuj Syal <syal.anuj@gmail>
2016-09-13 05:50:31 +0530

    fix

--- app/components/inspiration/inspirationCtrl.js
+++ app/components/inspiration/inspirationCtrl.js
@@ -14,38 +14,7 @@
     });
     app.controller('InspirationCtrl', ['$routeParams', '$uibModal', 'LocationService', controller]);
     function controller($scope, $routeParams, $uibModal, locationService) {
-        $scope.locationCards;
-        $scope.searchText;
-        var page = 0;
-
       
-            getLocationCards(page);
-        }
-            })
-        }
-        $scope.searchLocationByTags = function (tags) {
-            locationService.searchLocationByTag([tags], page, function (data) {
-                if (data) {
-                    $scope.locationCards = data;
-                    $scope.$apply();
-                    setTimeout(masonaryCall, 2000)
-                }
-            });
-        }
-
-        $scope.items = ['item1', 'item2', 'item3'];
-        $scope.open = function () {
-            var locationModalInstance = $uibModal.open({
-              
-                }
-            });
-            modalInstance.result.then(function (selectedItem) {
-                $scope.selected = selectedItem;
-            }, function () {
-                $log.info('Modal dismissed at: ' + new Date());
-            });
-        }
-    };
 
     
 })();
